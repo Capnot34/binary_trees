4cde6c23b6354a92b9e4b314fa128db53ad1e8dc
Capnot34 <jenniferamachree2@github.com>
2023-11-02 18:58:37 -0700

    solved task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -20,6 +20,20 @@ size_t binary_tree_height(const binary_tree_t *tree)
 }
 
 /**
+ * binary_tree_size - Calculate the size of a binary tree
+ * @tree: A pointer to the root node of the tree to measure
+ *
+ * Return: The size of the tree, or 0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree){
+	if (!tree)
+		return (0);
+
+	return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
+}
+
+
+/**
  * binary_tree_is_perfect - Check if a binary tree is perfect
  * @tree: A pointer to the root node of the tree to check
  *

dbaddd29d19c88314463a17ee757647da0cb1326
Capnot34 <jenniferamachree2@github.com>
2023-11-02 18:07:58 -0700

    could not debug taks 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,16 +10,10 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t depth = 0;
-
-	if (tree == NULL)
-		return (0);
-
-	while (tree->parent != NULL)
+	if (!tree)
 	{
-		depth++;
-		tree = tree->parent;
+		return (0);
 	}
 
-	return (depth);
+	 return (binary_tree_depth(tree->parent) + 1);
 }

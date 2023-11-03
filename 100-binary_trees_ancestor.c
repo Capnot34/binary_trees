7cf993a45ec32404b9318fd1bdd54507ac23bade
Ugwu Justine <ugwujustineobinna@gmaiil.com>
2023-11-03 04:51:25 +0100

    fix betty error

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -9,25 +9,26 @@
  * Return: A pointer to the lowest common
  * ancestor node, or NULL if not found
  */
-binary_tree_t *binary_trees_ancestor
-(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
 {
 	if (first == NULL || second == NULL)
-		return NULL;
+		return (NULL);
 
 	if (first == second)
-		return (binary_tree_t *)first;
+		return ((binary_tree_t *)first);
 
 	if (first == second->parent ||
-			(!first->parent && binary_tree_ancestor(second, first)))
-		return (binary_tree_t *)first;
+			(!first->parent && binary_trees_ancestor(second, first)))
+		return ((binary_tree_t *)first);
 	if (second == first->parent || (!second->parent &&
-				binary_tree_ancestor(first, second)))
-		return (binary_tree_t *)second;
+				binary_trees_ancestor(first, second)))
+		return ((binary_tree_t *)second);
 
 	if (first->parent == second->parent)
-		return (binary_tree_t *)first->parent;
+		return ((binary_tree_t *)first->parent);
 
 	binary_tree_t *ancestor = binary_trees_ancestor(first, second->parent);
-	return ancestor ? ancestor : binary_trees_ancestor(first->parent, second);
+
+	return (ancestor ? ancestor : binary_trees_ancestor(first->parent, second));
 }

55763b3dcccbffef7041595cceaf29a37d13c01b
Capnot34 <jenniferamachree2@github.com>
2023-11-02 17:51:58 -0700

    tackled task 10 error

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,10 +10,16 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	if (!tree)
-	{
+	size_t depth = 0;
+
+	if (tree == NULL)
 		return (0);
+
+	while (tree->parent != NULL)
+	{
+		depth++;
+		tree = tree->parent;
 	}
 
-	return (binary_tree_depth(tree->parent) + 1);
+	return (depth);
 }

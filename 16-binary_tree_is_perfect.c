b360ed37b7001a6b27a0c32df90b837df6a2ad97
Ugwu Justine <ugwujustineobinna@gmaiil.com>
2023-11-03 03:38:34 +0100

    fix betty error

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -25,7 +25,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
  *
  * Return: The size of the tree, or 0 if tree is NULL
  */
-size_t binary_tree_size(const binary_tree_t *tree){
+size_t binary_tree_size(const binary_tree_t *tree)
+{
 	if (!tree)
 		return (0);
 

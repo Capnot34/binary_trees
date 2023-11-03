31fb58fd0fd0fd45060970f3befb8199c62af4e0
Capnot34 <jenniferamachree2@github.com>
2023-11-03 01:55:22 -0700

    task 19

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,34 +1,53 @@
 #include "binary_trees.h"
 
+int binary_tree_is_descendant(const binary_tree_t *node, const binary_tree_t *ancestor);
+
 /**
- * binary_trees_ancestor - Find the lowest
- * common ancestor of two nodes
- * @first: A pointer to the first node
- * @second: A pointer to the second node
+ * binary_trees_ancestor - Find the lowest common ancestor of two nodes
+ * @first: Pointer to the first node
+ * @second: Pointer to the second node
  *
- * Return: A pointer to the lowest common
- * ancestor node, or NULL if not found
+ * Return: Pointer to the lowest common ancestor node or NULL
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-		const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
-	if (first == NULL || second == NULL)
+	binary_tree_t *ancestor;
+
+	if (!first || !second)
 		return (NULL);
 
 	if (first == second)
 		return ((binary_tree_t *)first);
 
-	if (first == second->parent ||
-			(!first->parent && binary_trees_ancestor(second, first)))
+	if (binary_tree_is_descendant(second, first))
 		return ((binary_tree_t *)first);
-	if (second == first->parent || (!second->parent &&
-				binary_trees_ancestor(first, second)))
+	if (binary_tree_is_descendant(first, second))
 		return ((binary_tree_t *)second);
 
-	if (first->parent == second->parent)
-		return ((binary_tree_t *)first->parent);
+	ancestor = binary_trees_ancestor(first->parent, second);
 
-	binary_tree_t *ancestor = binary_trees_ancestor(first, second->parent);
+	if (!ancestor) {
+		return (binary_trees_ancestor(first, second->parent));
+	}
 
-	return (ancestor ? ancestor : binary_trees_ancestor(first->parent, second));
+	return (ancestor);
+}
+
+/**
+ * binary_tree_is_descendant - Check if one node is a descendant of another
+ * @node: Potential descendant
+ * @ancestor: Potential ancestor
+ *
+ * Return: 1 if the first node is a descendant
+ * of the second, 0 otherwise
+ */
+int binary_tree_is_descendant(const binary_tree_t *node, const binary_tree_t *ancestor)
+{
+	if (!node || !ancestor)
+		return (0);
+
+	if (node == ancestor)
+		return (1);
+
+	return (binary_tree_is_descendant(node, ancestor->parent));
 }

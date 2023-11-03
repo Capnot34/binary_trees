df36961f03320e814a027d255cef13ab2350e365
Ugwu Justine <ugwujustineobinna@gmaiil.com>
2023-11-03 01:09:30 +0100

    fix betty error

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,17 +8,17 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    if (!node)
-    {
-        return (0); /* if the node is NULL, it cannot be a root */
-    }
+	if (!node)
+	{
+		return (0); /* if the node is NULL, it cannot be a root */
+	}
 
-    if (node->parent ==NULL)
-    {
-        return (1);
-    }
-        else
-        {
-            return (0);
-        }
+	if (node->parent == NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }

5a9f4ade44cea2045b785daa768a38ff23b08561
Ugwu Justine <ugwujustineobinna@gmaiil.com>
2023-11-03 01:05:49 +0100

    solve task 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binnary_tree_depth - a function that measures the depth of a node
+ * in a binary tree
+ *
+ * @tree: a pointer to the node to measure the depth
+ * Return: if null return 0
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (!tree)
+	{
+		return (0);
+	}
+
+	return (binary_tree_depth(tree->parent) + 1);
+}

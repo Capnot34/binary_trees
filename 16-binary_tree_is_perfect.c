231053ad54415631f5ba22e7ca36aad09e2ae5c5
Capnot34 <jenniferamachree2@github.com>
2023-11-02 23:17:18 -0700

    task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -11,18 +11,19 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
-
 	int left_height, right_height;
+
+	if (!tree)
+	{
+		return (0);
+	}
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
 	if (left_height == right_height)
 	{
-		if (binary_tree_is_full(tree))
-			return (1);
+		return (binary_tree_is_full(tree));
 	}
 
 	return (0);
